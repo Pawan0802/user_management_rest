429fe70f09ce7018e368a82361b4a7eef59d7815
Pawan Lamba <pawan88.lamba@gmail.com>
2020-08-07 19:50:04 -0400

    Tested Database Configuration

---
+++ config/include.php
@@ -0,0 +1,12 @@
+<?php
+
+$sitePath = $_SERVER['DOCUMENT_ROOT']."/user_management_rest/";
+
+include_once($sitePath.'config/Database.php');
+
+// Instantiate DB & connect
+// $database = new Database();
+// $db= $database->connect();
+
+
+?>
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-
 require 'vendor/autoload.php';
+include_once('config/include.php');
+
 Flight::route('/', function(){
     echo 'hello world!';
 });
+
+
+//start with registration only post for now
+
+
+
 
 Flight::start();
 ?>
